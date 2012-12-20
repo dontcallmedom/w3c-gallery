62ddb6f10f23b3b82e749d538b43135da5c3a6db
Dominique Hazael-Massieux <dom@w3.org>
2012-12-20 23:10:23 +0100

    testing POST picture

--- tests/app.js
+++ tests/app.js
@@ -1,16 +1,36 @@
 var assert = require("assert");
 var request = require("supertest");
-
 var app = require("../app.js").app;
+var mongoose = require("../app.js").db;
 
 describe('GET /', function(){
     it('should return HTML with 200 status', function(done){
 	request(app)
-            .get('/')
+	    .get('/')
 	    .expect('Content-Type', /html/)
-            .expect(200)
-            .end(function(err, res){
-		done();
-            });
+	    .expect(200)
+	    .end(function(err, res){
+		if (err) return done(err);
+		    done();
+	    });
+    });
+});
+
+describe('POST a picture and GET it', function() {
+    before(function() {
+	mongoose.connection.db.dropDatabase();
+    });
+    var pic;
+    describe('POST a picture', function() {
+	it('should return with 201 status and location header set', function(done) {
+	    request(app)
+		.post('/gallery')
+		.attach('image','test.jpg')
+		.expect(201)
+		.end(function(err, res){
+		    if (err) return done(err);		
+		    done();
+		});
+	});
     });
 });
