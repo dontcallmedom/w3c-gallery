7fc880b3b0320d8c7a43b1e1c71a64fadbbfd324
Dominique Hazael-Massieux <dom@w3.org>
2012-12-21 00:17:53 +0100

    sequential tests for POST/GET

--- tests/app.js
+++ tests/app.js
@@ -28,6 +28,29 @@ describe('POST a picture and GET it', function() {
 		.attach('image','test.jpg')
 		.expect(201)
 		.end(function(err, res){
+		    if (err) return done(err);
+		    done();
+		});
+	});
+    });
+    describe('GET it back', function() {
+	before(function(done) {
+	    request(app)
+		.post('/gallery')
+		.attach('image','test.jpg')
+		.expect(201)
+		.end(function(err, res) {
+		    if (err) return done(err);
+		    pic = res.headers["location"];
+		    done();
+		});
+	});
+	it('should return a JPEG image with 200 status', function(done) {
+	    request(app)
+		.get(pic)
+		.expect(200)
+		.expect('Content-Type', "image/jpeg")
+		.end(function(err, res){
 		    if (err) return done(err);		
 		    done();
 		});
